6ca1c64114537238114b3ef5802849e921c916bc
Lucas <210759480+nexapicore@users.noreply.github.com>
2025-05-17 08:05:35 +0200

    Create auth.js

---
+++ src/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const generateApiKey = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: '30s' });
+};
+
+const verifyApiKey = (token) => {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return null;
+  }
+};
+
+module.exports = { generateApiKey, verifyApiKey };
