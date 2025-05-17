9a0460fa2de4ecc3685619d948cc03a238b780c0
Lucas <210759480+nexapicore@users.noreply.github.com>
2025-05-17 08:32:59 +0200

    Update schedule.js

--- src/schedule.js
+++ src/schedule.js
@@ -14,4 +14,18 @@ const startRegeneration = () => {
   });
 };
 
+schedule.scheduleJob('*/5 * * * * *', async () => {
+  const users = await User.find();
+  const now = new Date();
+  for (const user of users) {
+    if (now - user.lastRegen >= user.regenInterval * 1000) {
+      user.apiKey = generateApiKey(user._id);
+      user.lastRegen = now;
+      await user.save();
+      console.log(`Regenerated API Key for ${user.email}`);
+    }
+  }
+});
+
+
 module.exports = { startRegeneration };
